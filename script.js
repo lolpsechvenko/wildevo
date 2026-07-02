8368c41e2def8bb4882c12074e22d4cc7a3a85e8
lolpsechvenko <lolpsechvenko@gmail.com>
2026-07-02 17:14:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let progress = 0;
 
+const fill = document.getElementById("fill");
+const text = document.getElementById("text");
+
+let load = setInterval(() => {
+
+    progress++;
+
+    fill.style.width = progress + "%";
+
+    if(progress < 60){
+        text.innerHTML = progress + "%";
+    }
+    else if(progress < 100){
+        text.innerHTML = "Connecting to server...";
+    }
+    else{
+        clearInterval(load);
+
+        text.innerHTML = "Connected!";
+
+        setTimeout(() => {
+            window.location.href = "lobby.html";
+        }, 800);
+    }
+
+}, 25);
